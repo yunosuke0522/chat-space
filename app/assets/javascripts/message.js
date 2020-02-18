b2c05e52d3a74bc5fbf82ba3bbd4422d055cbf31
yunosuke0522 <yunosuke.otake@di-v.co.jp>
2020-02-18 21:27:37 +0900

    全然コミットしてなくてごめんなさい。

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,73 @@
+$(function(){ 
+
+  function buildHTML(message){
+   if ( message.image ) {
+     var html =
+
+      `<div class="message" data-message-id=${message.id}>
+         <div class="message__upper-info">
+           <div class="message__upper-info__talker">
+             ${message.user_name}
+           </div>
+           <div class="message__upper-info__date">
+             ${message.date}
+           </div>
+         </div>
+         <div class="lower-message">
+           <p class="lower-message__content">
+             ${message.content}
+           </p>
+         </div>
+         <img src=${message.image} >
+       </div>`
+
+     return html;
+   } else {
+     var html =
+
+      `<div class="message" data-message-id=${message.id}>
+         <div class="message__upper-info">
+           <div class="message__upper-info__talker">
+             ${message.user_name}
+           </div>
+           <div class="message__upper-info__date">
+             ${message.date}
+           </div>
+         </div>
+         <div class="lower-message">
+           <p class="lower-message__content">
+             ${message.content}
+           </p>
+         </div>
+       </div>`
+     return html;
+   };
+ }
+
+$('#new_message').on('submit', function(e){
+ e.preventDefault();
+ var formData = new FormData(this);
+ var url = $(this).attr('action')
+ $.ajax({
+   url: url,
+   type: "POST",
+   data: formData,
+   dataType: 'json',
+   processData: false,
+   contentType: false
+ })
+  .done(function(data){
+    var html = buildHTML(data);
+    $('.messages').append(html);
+    $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+    $('form')[0].reset();
+  })
+  
+  .fail(function() {
+    alert("メッセージ送信に失敗しました");
+  })
+  .always(function(){
+    $('.send').prop("disabled", false);
+    });
+  });
+});
