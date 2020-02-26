cf248f22e6319b8301283b0f3d350e1083cb0a52
yunosuke0522 <yunosuke.otake@di-v.co.jp>
2020-02-26 15:11:29 +0900

    deleted these code

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,57 +1,48 @@
 $(function(){ 
-  var buildHTML = function(message) {
-    if (message.content && message.image) {
-      
-      var html = `<div class="message" data-message-id=` + message.id + `>` +
-        `<div class="message__upper-info">` +
-          `<div message__upper-info__talker">` +
-            message.user_name +
-          `</div>` +
-          `<div class="message__upper-info__date">` +
-            message.created_at +
-          `</div>` +
-        `</div>` +
-        `<div class="lower-message">` +
-          `<p class="lower-message__content">` +
-            message.content +
-          `</p>` +
-          `<img src="` + message.image + `" class="lower-message__image" >` +
-        `</div>` +
-      `</div>`
-    } else if (message.content) {
-      var html = `<div class="message" data-message-id=` + message.id + `>` +
-        `<div class="message__upper-info">` +
-          `<div class="message__upper-info__talker">` +
-            message.user_name +
-          `</div>` +
-          `<div class="message__upper-info__date">` +
-            message.created_at +
-          `</div>` +
-        `</div>` +
-        `<div class="lower-message">` +
-          `<p class="lower-message__content">` +
-            message.content +
-          `</p>` +
-        `</div>` +
-      `</div>`
-    } else if (message.image) {
-      var html = `<div class="message" data-message-id=` + message.id + `>` +
-        `<div class="message__upper-info">` +
-          `<div class="message__upper-info__talker">` +
-            message.user_name +
-          `</div>` +
-          `<div class="message__upper-info__date">` +
-            message.created_at +
-          `</div>` +
-        `</div>` +
-        `<div class="lower-message">` +
-          `<img src="` + message.image + `" class="lower-message__image" >` +
-        `</div>` +
-      `</div>`
-    };
-    return html;
-  };
 
+  function buildHTML(message){
+    if ( message.image ) {
+      var html =
+
+      `<div class="message" data-message-id=${message.id}>
+        <div class="message__upper-info">
+          <div class="message__upper-info__talker">
+            ${message.user_name}
+          </div>
+          <div class="message__upper-info__date">
+            ${message.date}
+          </div>
+        </div>
+        <div class="lower-message">
+          <p class="lower-message__content">
+            ${message.content}
+          </p>
+        </div>
+        <img src=${message.image} >
+      </div>`
+
+     return html;
+    } else {
+     var html =
+
+      `<div class="message" data-message-id=${message.id}>
+         <div class="message__upper-info">
+           <div class="message__upper-info__talker">
+             ${message.user_name}
+           </div>
+           <div class="message__upper-info__date">
+             ${message.date}
+           </div>
+         </div>
+         <div class="lower-message">
+           <p class="lower-message__content">
+             ${message.content}
+           </p>
+         </div>
+       </div>`
+     return html;
+    };
+  }
 
   $('#new_message').on('submit', function(e){
   e.preventDefault();
