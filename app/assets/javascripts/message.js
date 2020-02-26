0111ba46fbef5fea4cb148a9b91a323b169b8dfb
yunosuke0522 <yunosuke.otake@di-v.co.jp>
2020-02-26 12:27:08 +0900

    なんかわからんけど出来た

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -54,10 +54,10 @@ $(function(){
 
 
   $('#new_message').on('submit', function(e){
-  e.preventDefault();
-  var formData = new FormData(this);
-  var url = $(this).attr('action')
-
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+  
     $.ajax({
       url: url,
       type: "POST",
@@ -72,7 +72,7 @@ $(function(){
       $('form')[0].reset();
       $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
     })
-    
+      
     .fail(function() {
       alert("メッセージ送信に失敗しました");
     })
@@ -80,4 +80,30 @@ $(function(){
       $('.send').prop("disabled", false);
     });
   });
+
+  reloadMessages = function() {
+    var last_message_id = $('.message:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        var insertHTML = '';
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.messages').append(insertHTML);
+        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+      }
+    })
+    .fail(function() {
+      alert('error');
+    });
+  };
+if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+  setInterval(reloadMessages, 7000);
+}
 });
