23652217fa2c92ffec5b009b9d1e2cb3eb8a4c7f
yunosuke0522 <yunosuke.otake@di-v.co.jp>
2020-04-06 15:34:12 +0900

    modified these files

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -56,7 +56,7 @@ $(function(){
     })
     .done(function(data){
       var html = buildHTML(data);
-      $('#message').append(html);
+      $('.messages').append(html);
       $('form')[0].reset();
       $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
     })
@@ -92,6 +92,6 @@ $(function(){
     });
   };
   if (document.location.href.match(/\/groups\/\d+\/messages/)) {
-  setInterval(reloadMessages,1000);
+  setInterval(reloadMessages,7000);
   }
 });
--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -20,7 +20,7 @@ $(function() {
   function addDeleteUser(name, id) {
     let html = `
     <div class="chat-group-user clearfix" id="${id}">
-      <p class="chat-group-user__name">${name}</p>
+      <p class="chat-group-user__name">${nama}</p>
       <div class="user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn" data-user-id="${id}" data-user-name="${name}">削除</div>
     </div>`;
     $(".js-add-user").append(html);
