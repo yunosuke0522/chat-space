f35f3fa5a66dfb571dfeaac0f2b54fe9c5244585
yunosuke0522 <yunosuke.otake@di-v.co.jp>
2020-04-06 14:50:03 +0900

    modified this file

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -56,7 +56,7 @@ $(function(){
     })
     .done(function(data){
       var html = buildHTML(data);
-      $('.new_message').append(html);
+      $('#message').append(html);
       $('form')[0].reset();
       $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
     })
